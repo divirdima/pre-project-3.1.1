8a8ecf9d957d58d1b4ec1bbf21d8f10e227a6c52
Дмитрий Д <divirdima@gmail.com>
2022-06-20 01:16:16 +0300

    fix test path

--- src/test/java/com/springboot/DemoApplicationTests.java
+++ src/test/java/com/springboot/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class BootApplicationTests {
 
 	@Test
 	void contextLoads() {
